7ce05f4587047c4b3820de4f3f2e18b6cc7e0bab
Naman-ghost <singhnama64@gmail.com>
2025-04-06 15:33:56 +0530

    Added new files and updated content

--- my-crud-app/src/App.jsx
+++ my-crud-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';  // Import Navigate
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';  
 import Home from './Home';
 import Create from './Create';
 import Read from './Read';
@@ -8,6 +8,9 @@ import Login from './Login';
 import Delete from './Delete';
 import MainPage from './MainPage';  
 import MakePayments from './MakePayments';
+import PastPayments from './PastPayments';
+import FDs from './FDs';
+import FixedDeposits from './FixedDeposits';
 import './App.css';
 
 function App() {
@@ -34,13 +37,16 @@ function App() {
           path="/mainpage/:userId" 
           element={isLoggedIn ? <MainPage /> : <Navigate to="/login" />} 
         />
-        <Route path="/past-payment/:id" element={isLoggedIn ? <MakePayments /> : <Navigate to="/login" />}/>
+        <Route path="/past-payment/:userId" element={isLoggedIn ? <PastPayments /> : <Navigate to="/login" />} />
         <Route path="/make-payment/:id" element={isLoggedIn ? <MakePayments /> : <Navigate to="/login" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/read/:id" element={<Read />} />
         <Route path="/edit/:id" element={<Edit />} />
         <Route path="/delete/:id" element={<Delete />} />
+        <Route path="/fixed-deposits/:userId" element={<FixedDeposits />} />
+        <Route path="/fds/:userId" element={<FixedDeposits />} />
+
       </Routes>
     </BrowserRouter>
   );
